4c2de40d800b8b1bd37b445235aea56eb678ebfa
Konrad Lekawski <konrad.jakub.lekawski@gmail.com>
2022-12-18 17:48:18 +0100

    Template with spring boot

---
+++ src/main/java/com/example/spring_springboot/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.example.spring_springboot.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.example.spring_springboot.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        System.out.println("Home Page Requested, locale = " + locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String user(User user, Model model) {
+        System.out.println("User Page Requested");
+        model.addAttribute("userName", user.getUserName());
+        return "user";
+    }
+}
